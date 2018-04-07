969084df725809516107ffcba59d910083471bea
River Park Place <theriverparkplace@gmail.com>
2018-04-08 06:20:34 +0800

    'versionLock'

--- index.js
+++ index.js
@@ -143,7 +143,6 @@ const Toolkit = (opts) => {
             });
           })
           .then(() => {
-            console.log(data);
             resolve(data);
           })
           .catch(reject);
